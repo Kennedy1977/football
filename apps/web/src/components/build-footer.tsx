d1afa10bfed9b204adb4564a5b87e89b67658b76
andrew <andrew@umsl.co.uk>
2026-03-09 00:31:40 +0000

    copyright / build notice

--- apps/web/src/components/build-footer.tsx
+++ apps/web/src/components/build-footer.tsx
@@ -1,5 +1,5 @@
 import { APP_VERSION_LABEL } from "../lib/build-meta";
 
 export function BuildFooter() {
-  return <div className="build-footer">© Andrew Kennedy · {APP_VERSION_LABEL}</div>;
+  return <div className="build-footer">Copyright &copy;2026 Andrew Kennedy · {APP_VERSION_LABEL}</div>;
 }
